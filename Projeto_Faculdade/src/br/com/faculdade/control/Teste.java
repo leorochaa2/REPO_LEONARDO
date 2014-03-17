12a8dd0a725d6efeebe16fbec798bb73182ed964
Leonardo <leorochaa2@gmail.com>
2014-03-17 19:34:45 -0300

    To na faculdade

--- Projeto_Faculdade/src/br/com/faculdade/control/Teste.java
+++ Projeto_Faculdade/src/br/com/faculdade/control/Teste.java
@@ -3,6 +3,7 @@ package br.com.faculdade.control;
 public class Teste {
 
 	public static void main(String[] args) {
-		System.out.println("Primeiro Commit!");
+		System.out.println("Primeiro Commit!kkkkkkkkkkkkkkkk");
+		
 	}
 }
